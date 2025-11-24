9f771f07c101ddac710d44d7b2c97ab3d602b30c
Rachit Gupta <2020ucp1736@mnit.ac.in>
2025-11-24 22:53:13 +0530

    Initial ProjectPulse setup

---
+++ src/test/java/com/example/ProjectPulse/ProjectPulseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ProjectPulse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectPulseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
